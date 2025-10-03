dba5a4939dd97c5c3b8d154707727cdbaa32850c
Joao Guilherme Lima Souza <jlima-so@c2r1s8.42lisboa.com>
2025-10-03 15:43:51 +0100

    added exercices

---
+++ Module-02/ex01/Fixed.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlima-so <jlima-so@student.42lisba.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 15:20:20 by jlima-so          #+#    #+#             */
+/*   Updated: 2025/10/03 15:31:56 by jlima-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (value);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	value = raw;
+}
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	value = 0;
+}
+
+Fixed::Fixed(const Fixed &obj)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	value = obj.getRawBits();
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Default desstructor called" << std::endl;
+	value = 0;
+}
+
+void	Fixed::operator=(const Fixed& obj)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->value = obj.getRawBits();
+}
