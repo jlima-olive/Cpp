23226d082cedda1286bf90736791d94b1e409a9c
jlima-olive <namejojo0214@gmail.com>
2025-10-06 10:09:29 +0100

    msg

---
+++ Module-02/ex02/Fixed.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namejojo <namejojo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 15:20:22 by jlima-so          #+#    #+#             */
+/*   Updated: 2025/10/06 10:07:02 by namejojo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FIXED_HPP
+# define FIXED_HPP
+
+# ifndef LITERAL
+#  define LITERAL 8
+# endif
+
+#include <iostream>
+
+// std::basic_ostream<char>& operator<<(std::basic_ostream<char> &out, const Fixed &obj);
+
+class	Fixed
+{
+private:
+	int					value;
+	static const int	LIT = LITERAL;
+public:
+	Fixed();
+	Fixed(const Fixed &obj);
+	void	operator=(const Fixed& obj);
+	Fixed(const int numb);
+	Fixed(const float numb);
+	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+	std::basic_ostream<char>& operator<<(std::basic_ostream<char> &out, const Fixed &obj);
+};
+
+#endif
