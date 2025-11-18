1b15cd11fd1d5cb758a0c5ec6969d4a5d26146be
jlima-olive <namejojo0214@gmail.com>
2025-11-18 14:48:57 +0000

    update

--- Module-04/ex01/Animal.cpp
+++ Module-04/ex01/Animal.cpp
@@ -6,11 +6,12 @@
 /*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 12:58:57 by namejojo          #+#    #+#             */
-/*   Updated: 2025/10/21 13:56:08 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/11/18 14:39:56 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
+
 
 void	Animal::makeSound(void) const
 {
@@ -22,6 +23,19 @@ Animal::Animal()
 	sound = "*lawn mower noises*";
 	type = "huh?";
 	std::cout << "Animal default constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal &obj) : 
+sound(obj.sound),
+type(obj.type)
+{
+}
+
+Animal &Animal::operator=(const Animal &obj)
+{
+	this->sound = obj.sound;
+	this->type = obj.type;
+	return *this;
 }
 
 Animal::~Animal()
