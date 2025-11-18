0a3f9160f6863730c13b40eee52c31df2d6350a5
jlima-olive <namejojo0214@gmail.com>
2025-11-18 14:52:59 +0000

    update

--- Module-04/ex00/main.cpp
+++ Module-04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 13:00:23 by namejojo          #+#    #+#             */
-/*   Updated: 2025/11/18 14:12:12 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/11/18 14:52:36 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,28 @@
 
 int main()
 {
-/* const Animal* meta = new Animal();
-const Animal* j = new Dog();
-const Animal* i = new Cat();
+	{
+		const Animal* meta = new Animal();
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
 
-std::cout << j->getType() << " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
-return 0; */
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound(); //will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
+		delete j;
+		delete i;
+	}
+	const WrongAnimal* j = new WrongAnimal();
+	const WrongAnimal* i = new WrongCat();
 
-// const WrongAnimal* j = new WrongDog();
-const WrongAnimal* meta = new WrongAnimal();
-const WrongAnimal* i = new WrongCat();
+	std::cout << i->getType() << std::endl;
+	std::cout << j->getType() << std::endl;
+	i->makeSound();//will not output the cat sound!
+	j->makeSound();
+	delete j;
+	delete i;
 
-// std::cout << j->getType() << " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound();
-meta->makeSound();
-return 0;
+	return 0;
 }
