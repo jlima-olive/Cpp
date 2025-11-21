8b9fcd0455d78ebdd3cca476e879f23f2e4fb7b8
jlima <jlima-so@c2r8s8.42lisboa.com>
2025-11-21 12:54:40 +0000

    update

--- Module-04/ex01/Dog.hpp
+++ Module-04/ex01/Dog.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Dog.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jlima-so <jlima-so@student.42lisba.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 12:57:18 by namejojo          #+#    #+#             */
-/*   Updated: 2025/11/18 15:11:31 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/11/21 12:53:39 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ public:
 	Dog();
 	Dog(const Dog &obj);
 	Dog &operator=(const Dog &obj);
-
 	~Dog();
 };
 
