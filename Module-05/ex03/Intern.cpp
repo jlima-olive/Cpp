6c52a7e526cd39e1fda7413bd45f1608bb85568a
jlima-so <jlima.olive@gmail.com>
2025-12-11 18:24:06 +0000

    update

--- Module-05/ex03/Intern.cpp
+++ Module-05/ex03/Intern.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Intern.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jlima-so <jlima-so@student.42lisba.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 13:00:24 by jlima-so          #+#    #+#             */
-/*   Updated: 2025/11/26 13:15:06 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/12/11 18:24:01 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,41 @@
 
 Intern::Intern()
 {
-	std::cout << "Inter default constructor called" << std::endl;
+	std::cout << "Intern default constructor called" << std::endl;
 }
 
 Intern::Intern(Intern &obj)
 {
-	std::cout << "Inter copy constructor called" << std::endl;
+	std::cout << "Intern copy constructor called" << std::endl;
 }
 
 Intern &Intern::operator=(Intern &obj)
 {
-	std::cout << "Inter copy assignment operator called" << std::endl;
+	std::cout << "Intern copy assignment operator called" << std::endl;
 }
 
 Intern::~Intern()
 {
-	std::cout << "Inter destructor called" << std::endl;
+	std::cout << "Intern destructor called" << std::endl;
 }
 
 AForm *Intern::makeForm(const std::string &form, const std::string &target) const
 {
 	if (form == "PresidentialPardonForm")
 	{
-		std::cout << "Inter created a PresidentialPardonForm" << std::endl;
+		std::cout << "Intern created a PresidentialPardonForm" << std::endl;
 		return (new PresidentialPardonForm(target));
 	}
 	if (form == "ShrubberyCreationForm")
 	{
-		std::cout << "Inter created a ShrubberyCreationForm" << std::endl;
+		std::cout << "Intern created a ShrubberyCreationForm" << std::endl;
 		return (new ShrubberyCreationForm(target));
 	}
 	if (form == "RobotomyRequestForm")
 	{
-		std::cout << "Inter created a RobotomyRequestForm" << std::endl;
+		std::cout << "Intern created a RobotomyRequestForm" << std::endl;
 		return (new RobotomyRequestForm(target));
 	}
-	std::cout << "The Inter couldnt find that specific form, maybe we should pay him even less??" << std::endl;
+	std::cout << "The Intern couldnt find that specific form, maybe we should pay him even less??" << std::endl;
 }
 
