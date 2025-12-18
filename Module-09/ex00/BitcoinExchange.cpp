99b1644d3603f2294e3fdfedae9536de1eb05d23
jlima-olive <namejojo0214@gmail.com>
2025-12-18 20:42:32 +0000

    working on ex00

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -1,1 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 17:54:01 by jlima-so          #+#    #+#             */
+/*   Updated: 2025/12/18 20:36:52 by jlima-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+# include <BitcoinExchange.hpp>
+
+BitcoinExchange::BitcoinExchange()
+{
+	init = 0;
+	std::cout << "BitcoinExchange default constructor called" << std::endl;
+}
+	
+BitcoinExchange::BitcoinExchange(BitcoinExchange &obj) :
+datab(obj.datab)
+{
+	std::cout << "BitcoinExchange argument constructor called" << std::endl;
+	init = 1;
+}
+	
+BitcoinExchange::BitcoinExchange(std::string file, char ch)
+{
+	std::cout << "BitcoinExchange copy operator called" << std::endl;
+	readDataBase(file, ch);
+}
+	
+BitcoinExchange::~BitcoinExchange()
+{
+	std::cout << "BitcoinExchange default destructor called" << std::endl;
+}
+	
+BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange &ref)
+{
+	std::cout << "BitcoinExchange copy assignment operator called" << std::endl;
+	datab = ref.datab;
+	return (*this);
+}
+	
+void	BitcoinExchange::readDataBase(std::string file, char ch)
+{
+	std::string		key;
+	std::ifstream	infile(file.c_str());
+	int				value;
+	std::stringstream	stream;
+
+	if (!infile.is_open())
+	{
+		std::cout  << "Coudn't open file:" << file.c_str() << std::endl;
+		throw (DataBaseError());
+	}
+	while (getline(infile, key))
+	{
+		int	pos;
+
+		for (std::string::iterator it = key.begin(); it != key.end(); it++)
+			if (*it == ' ')
+				key.erase(it);
+		pos = key.find(ch);
+		if (pos != -1)
+		{
+			std::cout << "substr:	" << key.substr(pos) << '|' << std::endl;
+			stream << key.substr(pos);
+			stream >> value;
+			std::cout << "value:	" << value << std::endl;
+			datab.insert(std::make_pair(key.substr(0, pos), value));
+			stream.clear();
+		}
+		// std::cout << "4" << std::endl;
+	}
+	for (std::map<std::string, float>::iterator it = datab.begin(); it != datab.end(); it++)
+		std::cout << '|' << it->first << '|' << ' ' << '|' << it->second << '|' << std::endl;
+		
+}
+void	BitcoinExchange::outputDataBaseMult(BitcoinExchange &db)
+{
+	
+}
+	
+const char *BitcoinExchange::ObjectNotInitialized::what() const throw()
+{
+	return ("Object was not initialized");
+}
+	
+const char *BitcoinExchange::DataBaseError::what() const throw()
+{
+	return ("There was an error with the Data Base");
+}
--- Module-09/ex00/BitcoinExchange.hpp
+++ Module-09/ex00/BitcoinExchange.hpp
@@ -1,1 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 17:59:15 by jlima-so          #+#    #+#             */
+/*   Updated: 2025/12/18 19:45:19 by jlima-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <map>
+# include <fstream>
+# include <string>
+# include <iterator>
+# include <sstream>
+
+class BitcoinExchange
+{
+private:
+	std::map<std::string, float>	datab;
+	int						init;
+public:
+	BitcoinExchange();
+	BitcoinExchange(std::string file, char ch);
+	BitcoinExchange(BitcoinExchange &ref);
+	~BitcoinExchange();
+	BitcoinExchange &operator=(BitcoinExchange &ref);
+
+	void	readDataBase(std::string file, char ch);
+	void	outputDataBaseMult(BitcoinExchange &db);
+	
+	class ObjectNotInitialized : public std::exception
+	{
+		virtual const char *what() const throw();
+	};
+	class DataBaseError : public std::exception
+	{
+		virtual const char *what() const throw();
+	};
+};
+
+#endif
