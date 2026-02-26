b1027fdb82dc5e4ede31b2389072189965d98636
jlima-so <jlima.olive@gmail.com>
2026-02-26 12:06:15 +0000

    update

--- Module-07/ex02/main.cpp
+++ Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jlima-so <jlima-so@student.42lisba.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 20:18:52 by jlima-so          #+#    #+#             */
-/*   Updated: 2026/02/26 12:05:03 by jlima-so         ###   ########.fr       */
+/*   Updated: 2026/02/26 12:06:05 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,5 @@ int main(void)
 	std::cout << array[9];
 	std::cout << "array[9] = '" << array[9] << "'" << std::endl;
 	std::cout << "carray[9] = '" << carray[9] << "'" << std::endl;
-	// char str[12];
-// 
-	// strcpy(str, "hello world");
-	// std::cout << str << std::endl;
-	// iter(str, 12, sub<char &>);
-	// std::cout << str << std::endl;
-	// iter(str, 12, add);
-	// std::cout << str << std::endl;
+
 }
