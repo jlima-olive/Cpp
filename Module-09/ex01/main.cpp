029215ba8ed7beade37ddae00afb33f21e8d5483
jojo <namejojo0214@gmail.com>
2026-03-12 19:34:27 +0000

    cpp09/ex02 still has prints

--- Module-09/ex01/main.cpp
+++ Module-09/ex01/main.cpp
@@ -2,8 +2,10 @@
 
 int main (int ac, char **av)
 {
-	if (ac != 2)
+	if (ac < 2)
 		return (std::cout << "No expression input" << std::endl, 1);
+	else if (ac > 2)
+		return (std::cout << "Not a valid input" << std::endl, 1);
 	RPN poslishmath;
 
 	// std::cout << *(av + 1) << std::endl;
